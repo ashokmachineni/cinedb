d4ee3c626ee60cac264b2b2093d73f0ffefdab08
ashokmachineni <ashok5679m@gmail.com>
2020-05-30 15:52:54 +0100

    added header component and styles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Provider } from "react-redux";
 import store from "./redux/store";
-import "./App.css";
+import "./App.scss";
+import Header from "./components/header/Header";
 //http://react-cinedb-dev.s3-website-us-east-1.amazonaws.com
 
 const App = () => {
   return (
     <Provider store={store}>
+      <Header />
       <div className="app">
         <h1>React Redux setup finished</h1>
       </div>
